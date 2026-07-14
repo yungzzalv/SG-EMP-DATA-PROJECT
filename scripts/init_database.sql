8a21d34fdf10edc037280f08d525094d00e5092c
yungzzalv <yungzzalv@gmail.com>
2026-07-14 10:43:48 +0800

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,12 @@
+==============================================
+  Header Comment : brief description of the purpose about the script,flow,
+  Warning : precaution of what will happen when you run this script, especially when you have drop or truncate
+=============================================
+
+use SG_EMP_DATA;
+
+create schema BRONZE;
+go
+create schema SILVER;
+GO
+create schema GOLD;
